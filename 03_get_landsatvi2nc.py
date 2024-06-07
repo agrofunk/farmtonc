7ea366f07d1ec3790236af741fa1329018f79597
Denis Mariano <denis@seca.space>
2024-06-07 00:54:40 -0300

    concat ok, quan indices ok

--- 03_get_landsatvi2nc.py
+++ 03_get_landsatvi2nc.py
@@ -1,19 +1,4 @@
 #%%
-run = 'f3'
-# f1 varios filtros, only L8 e L9, break em 2017
-#   f1 salvando em float32
-
-# f2 = f1 + landsat 5 e 7
-#  tentar try except:
-
-# Anselmo Busatto
-#farm_file = '/home/geodata/Clientes/0FARMS/MG-3102605-B4D344DBFD874F44906FCC0A5E0DCE36/CAR.gpkg'
-
-# Barreiras BA
-farm_file = '/home/geodata/Clientes/0FARMS/FabioRicardi-Barreiras_BA/CARS-Fazenda_Savana.gpkg'
-folder_nc = farm_file.split('CAR.gpkg')[0] + f'nc/{run}/'
-print(folder_nc)
-
 import os
 import boto3
 import rasterio as rio
@@ -26,6 +11,27 @@ from datetime import date, datetime
 
 from utils import *
 import matplotlib.pyplot as plt
+#%%
+run = 'f5'
+
+farm_file = '/home/geodata/Clientes/0FARMS/FabioRicardi-Barreiras_BA/vetorial/CARs-Fazenda_Savana.gpkg'
+farm_file = '/home/geodata/Clientes/0FARMS/MG-3102605-B4D344DBFD874F44906FCC0A5E0DCE36/CAR.gpkg'
+layer = 'car'
+folderout = '/home/geodata/Clientes/0FARMS/FabioRicardi-Barreiras_BA/nc/'
+folderout = '/home/geodata/Clientes/0FARMS/MG-3102605-B4D344DBFD874F44906FCC0A5E0DCE36/nc/'
+
+#  Satellite imagery query params
+today = date.today()
+datetime_rangefull = str(f"2015-06-20/{str(today)}") #break  2017
+#datetime_rangefull = str(f"1985-06-20/2013-06-20") #break  2017
+max_cloud = 100
+#bucketname = 'sanca'
+satellite = 'Landsat'
+platforms = ["LANDSAT_5","LANDSAT_8", "LANDSAT_9"] #"LANDSAT_7",
+
+
+folder_nc = f'{folderout}{run}/'
+print(folder_nc)
 
 # Set environment and create AWS Session
 os.environ['CURL_CA_BUNDLE'] = '/etc/ssl/certs/ca-certificates.crt'
@@ -37,19 +43,13 @@ print(aws_session)
 
 # open Farm
 try:
-    farm = gpd.read_file(farm_file, layer='AREA_IMOVEL_1')
+    farm = gpd.read_file(farm_file, layer=layer)
 except:
     farm = gpd.read_file(farm_file)
 
 bbox = get_bbox(farm)
 
-#  Satellite imagery query params
-today = date.today()
-# datetime_rangefull = str(f"2013-06-20/{str(today)}") #break  2017
-datetime_rangefull = str(f"1985-06-20/2013-06-20") #break  2017
-max_cloud = 100
-#bucketname = 'sanca'
-satellite = 'Landsat'
+
 
 # %% GET COLLECTION AND PARAMETERS
 URL = 'https://landsatlook.usgs.gov/stac-server'
@@ -60,26 +60,28 @@ print(collection)
 
 query_params = {
         "eo:cloud_cover": {"lt": max_cloud},
-        "platform": {"in": ["LANDSAT_5","LANDSAT_7","LANDSAT_8", "LANDSAT_9"]},
+        "platform": {"in": platforms},
        "landsat:collection_category": { "in": ['T1']}
                 }
 
-assets = ['red', 'blue', 'nir08', 'swir16' ] #'green', 'qa_pixel'
+assets = ['red', 'blue', 'nir08', 'swir16'] #'green', 'qa_pixel'
 
 y0 = datetime.strptime(datetime_rangefull.split('/')[0],'%Y-%m-%d').year
 yf = datetime.strptime(datetime_rangefull.split('/')[1],'%Y-%m-%d').year
 m0 = str(datetime.strptime(datetime_rangefull.split('/')[0],'%Y-%m-%d').month).zfill(2)
 mf = str(datetime.strptime(datetime_rangefull.split('/')[1],'%Y-%m-%d').month).zfill(2)
-d0 = str(datetime.strptime(datetime_rangefull.split('/')[0],'%Y-%m-%d').day).zfill(2)
-df = str(datetime.strptime(datetime_rangefull.split('/')[1],'%Y-%m-%d').day).zfill(2)
+d0 = str(datetime.strptime(datetime_rangefull.split('/')[0],'%Y-%m-%d').day+1).zfill(2)
+df = str(datetime.strptime(datetime_rangefull.split('/')[1],'%Y-%m-%d').day-2).zfill(2)
+if int(d0)>31: d0 = '30'
+if int(df)<1: df = '01'
 # %%
 Path(folder_nc).mkdir( parents = True, exist_ok = True)
 for ano in range(y0,yf):
     '''
         TODO falta um globals aqui pra player com os dados
     '''
-
-    datetime_range = f'{ano}-{m0}-{d0}/{ano+1}-{m0}-{d0}'
+    d0_ = str(int(d0)-2).zfill(2)
+    datetime_range = f'{ano}-{m0}-{d0}/{ano+1}-{m0}-{d0_}'
     if yf == (ano+1):
         datetime_range = f'{ano}-{m0}-{d0}/{ano+1}-{mf}-{df}'
     print(ano, datetime_range)
@@ -95,22 +97,24 @@ for ano in range(y0,yf):
                                         aws_session, assets)
         ds = dropper( ds , sat = satellite ) 
 
+
         # é aqui que a filtragem acontece
         ds2 = ds.copy()
 
         # valores extremos 
-        ds2 = xr.where(ds2 > 40000, np.nan, ds2)
-        ds2 = xr.where(ds2 < 5000, np.nan, ds2)
+
         for asset in assets:
-            quantiles = [0.01,0.1,0.25,0.5,0.75,0.9,0.99]
+            ds2[asset] = xr.where(ds2[asset] > 40000, np.nan, ds2[asset])
+            ds2[asset] = xr.where(ds2[asset] < 5000, np.nan, ds2[asset])
+
             print(asset)
-            print(quantiles)
+            quantiles = [0.01,0.1,0.25,0.5,0.75,0.9,0.99]
             print(np.nanquantile(ds2[asset],quantiles))
             ds2[asset] = xr.where(ds2[asset] < np.nanquantile(ds2[asset],[0.01]), np.nan, ds2[asset])
             ds2[asset] = xr.where(ds2[asset] > np.nanquantile(ds2[asset],[0.99]), np.nan, ds2[asset])
             if asset == 'blue':
-                ds2[asset] = xr.where(ds2[asset] > np.nanquantile(ds2[asset],[0.76]), np.nan, ds2[asset])
-        
+                ds2[asset] = xr.where(ds2[asset] > np.nanquantile(ds2[asset],[0.84]), np.nan, ds2[asset])
+            
         # interpolate_na
         ds2 = ds2.chunk(dict(time=-1))
         ds2 = ds2.interpolate_na(dim="time",
@@ -123,13 +127,15 @@ for ano in range(y0,yf):
         ds2 = ds2.rolling(time=w, center=True).mean(skipna=True)
 
 
-        # REPROJECTION
+        # # REPROJECTION
         print(f'reprojecting cube for {datetime_range}')
         ds2 = ds2.rio.write_crs('epsg:4326')
         ds2 = ds2.rio.reproject('EPSG:4326')
         ds2 = ds2.rename({'x': 'longitude','y': 'latitude'})
         print('reprojecting... done')
 
+        ds2.to_netcdf(f'{folder_nc}/raw_{datetime_range_name}.nc')
+        print(f'>> {folder_nc}/raw_{datetime_range_name}.nc')
 
         # CALCULATE indices
         ndvi = NDVI( ds2 )
@@ -150,20 +156,11 @@ for ano in range(y0,yf):
         bsi.to_netcdf(f'{folder_nc}/bsi_{t0}-{t1}_{n}.nc')
         print('> bsi saved') 
 
-        # # CALCULATE climatology
-        # ndvi_mean, ndvi_std = climatology(ndvi)
-        # bsi_mean, bsi_std = climatology(bsi)
-
-        # # CALCULATE Zscore and anomalies
-        # ndvi_anom, ndvi_z = zscore( ndvi, 
-        #                         how = 'month')
-        # bsi_anom, bsi_z = zscore( bsi, 
-        #                         how = 'month')
-
     except:
         print(f'{datetime_range} FAILED')
 
-# # # %%
+
+# %% PLOTTING STUFF
 # # ndvi.clip(0,1000)
 # # # %% TEMOS UM ds
 
@@ -255,3 +252,5 @@ for ano in range(y0,yf):
 #     plt.title(asset)
 #     plt.title(asset); plt.grid();plt.show(); plt.close()
 
+
+# %%
